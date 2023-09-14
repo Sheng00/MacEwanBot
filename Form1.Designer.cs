b4d2575ab650369040283c86775b8725446a059f
Sheng00 <37249160+Sheng00@users.noreply.github.com>
2023-09-13 20:53:03 -0600

    Add files via upload
    This project is in a Work in Progress!!!
    Automatically scrapes assignments and due dates from the MacEwan Meskanas webpage, and puts them into an organized table.

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace MacEwan_Auto_scheduler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(393, 346);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Assignments Due\n---------------------\n\nENGL 102\nSeptember 20th - Quiz 1 Quiz 2 Qu" +
+    "iz 3\n\n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save to .txt file";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.statusLabel);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(408, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(380, 334);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gather Bot";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start service";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(132, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Kill Service";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Created by Shaen Gignac";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(17, 78);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(224, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Scrape (Use after signed in)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(23, 119);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(73, 13);
+            this.statusLabel.TabIndex = 6;
+            this.statusLabel.Text = "Status: Offline";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(235, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Gignacs";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 193);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(235, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "Shaen7803002812";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Grant MacEwan Due Assignment Viewer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V114.CSS;
+using OpenQA.Selenium.Edge;
+using System.Threading;
+
+namespace MacEwan_Auto_scheduler
+{
+    public partial class Form1 : Form
+    {
+
+        string buildData = "Assignments Due\n---------------------\n";
+        public int index = 0;
+
+        IWebDriver driver;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            driver = new EdgeDriver();
+            driver.Url = "https://meskanas.macewan.ca/my/courses.php";
+            driver.Navigate();
+            IWebElement loginElement = driver.FindElement(By.ClassName("login-identityprovider-btn"));
+            loginElement.Click();
+            IWebElement user = driver.FindElement(By.Id("username"));
+            IWebElement pass = driver.FindElement(By.Id("password"));
+            IWebElement login = driver.FindElement(By.ClassName("form-button"));
+
+            user.Click();
+            user.SendKeys(textBox1.Text);
+            pass.Click();
+            pass.SendKeys(textBox2.Text);
+            login.Click();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var element = driver.FindElements(By.ClassName("dashboard-card-img"));
+
+
+                for (int i = 0 + index; i < element.Count; i++)
+                {
+
+                    //Thread.Sleep(1000);
+                    element = driver.FindElements(By.ClassName("dashboard-card-img"));
+                    var names = driver.FindElements(By.ClassName("multiline"));
+                    string className = names[i].Text;
+
+                    // Click class and scrape assignments
+                    element[i].Click();
+
+                    var assignments = driver.FindElements(By.ClassName("instancename"));
+
+
+                    //var assignment_selector_datas = driver.FindElements(By.ClassName("sectionname"));
+                    if (driver.PageSource.Contains("Assignments"))
+                    {
+                        //for (int y = 0; y < assignment_selector_datas.Count; y++)
+                        {
+                            //string datas = assignment_selector_datas[y].Text.ToLower();
+                            //if (datas.Contains("assignment") || datas == "assignment" || datas.Contains("lab") || datas.Contains("qui"))
+                            {
+
+                                richTextBox1.Text += "Assignments SECTION FOUND in " + className + "\n";
+                                //for (int x = 0; x < assignments.Count; x++)
+                                {
+                                    /*if (assignments[x].Text.Contains("Available"))
+                                    {
+                                        richTextBox1.Text += $"\n{className}\n---------------------\n{assignments[x].Text}";
+                                    }*/
+                                    //else
+                                    //{
+                                    /*assignments = driver.FindElements(By.ClassName("instancename"));
+                                    string txt = assignments[x].Text.ToLower();
+                                    if (txt.Contains("assignment") || txt.Contains("questions") || txt.Contains("quiz") || txt.Contains("lab"))
+                                    {
+                                        richTextBox1.Text += $"\n{assignments[x].Text}";
+                                    }*/
+
+
+                                    var possibleAssignments = driver.FindElements(By.ClassName("activityname"));
+
+                                    string curURL = driver.Url;
+
+                                    for (int z = 0; z < possibleAssignments.Count; z++)
+                                    {
+                                        //Thread.Sleep(300);
+                                        possibleAssignments = driver.FindElements(By.ClassName("activityname"));
+                                        string name_ = possibleAssignments[z].Text;
+                                        if (name_.ToLower().Contains("lab") || name_.ToLower().Contains("assignmen") || name_.ToLower().Contains("qui") || name_.ToLower().Contains("projec") || name_.ToLower() == "lab")
+                                        {
+                                            possibleAssignments[z].Click();
+                                            richTextBox1.Text += $"\n{name_}\n------------\n";
+                                            string DueDateAndInformation = "";
+                                            string opt_1 = "no-overflow";
+                                            string opt_2 = "description-inner";
+                                            string opt_3 = "activity-dates";
+
+                                            try
+                                            {
+                                                DueDateAndInformation = driver.FindElement(By.ClassName(opt_1)).Text;
+                                                richTextBox1.Text += DueDateAndInformation + "\n";
+                                            }
+                                            catch (Exception ex)
+                                            {
+                                                richTextBox1.Text += $"\nNo information\n";
+                                            }
+
+                                            
+                                                try
+                                                {
+                                                    DueDateAndInformation = driver.FindElement(By.ClassName(opt_2)).Text;
+                                                richTextBox1.Text += DueDateAndInformation + "\n";
+
+                                            }
+                                            catch (Exception ex)
+                                                {
+                                                    //richTextBox1.Text += $"\nNo information\n";
+                                                }
+
+                                            try
+                                            {
+                                                DueDateAndInformation = driver.FindElement(By.ClassName(opt_3)).Text;
+                                                richTextBox1.Text += DueDateAndInformation + "\n";
+
+                                            }
+                                            catch (Exception ex)
+                                            {
+                                                //richTextBox1.Text += $"\nNo information\n";
+                                            }
+
+
+                                        }
+                                        driver.Url = curURL;
+                                        driver.Navigate();
+                                        // }
+
+
+
+
+                                    }
+                                    // }
+
+
+
+                                }
+                            }
+                        }
+                        /* else
+                         {
+                             richTextBox1.Text += "No assigments in " + className;
+
+                         }*/
+
+
+                    }
+                    driver.Url = "https://meskanas.macewan.ca/my/courses.php";
+                    driver.Navigate();
+                    index++;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR... SCRAPE AGAIN!");
+                index--;
+
+            }
+
+            }
+        }
+    }
